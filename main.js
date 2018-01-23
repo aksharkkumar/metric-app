4c38d6569413d8a621aa24c423078a01dd615bc7
Akshar Kumar <ak.kumar93@gmail.com>
2018-01-23 00:07:36 -0800

    adds photon ui

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron')
+const {app,BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
 
@@ -10,13 +10,13 @@ function createWindow() {
     win = new BrowserWindow({width:800,height:600})
     //load the index.html of the app
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
+        pathname: path.join(__dirname, 'app/index.html'),
         protocol: 'file:',
         slashes: true
     }))
 
     //Open the DevTools
-    win.webContents.openDevTools()
+    // win.webContents.openDevTools()
 
     //Emitted when the window is closed.
     win.on('closed', () => {
